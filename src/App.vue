1ea9c586e94ebda2d45d0c762a7b7f3629476b26
SimanyeSTS <simanyesomdaka@gmail.com>
2024-07-02 14:23:27 +0200

    HomeViewandApp

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="wrapper">
+    <NavBar />
+    <main>
+      <router-view />
+      <HomeSection />
+      <About />
+      <ResumeSection />
+      <ProjectSection />
+      <TestimonialsSection />
+    </main>
+    <FooterComp />
+  </div>
 </template>
 
+<script>
+import NavBar from '@/components/NavBar.vue'
+import AboutSection from '@/components/AboutSection.vue'
+import FooterComp from '@/components/FooterComp.vue'
+import ProjectsSection from '@/components/ProjectsSection.vue'
+import ResumeSection from '@/components/ResumeSection.vue'
+import TestimonialsSection from '@/components/TestimonialsSection.vue'
+import HomeSectionVue from './components/HomeSection.vue'
+
+export default {
+  components:
+  NavBar,
+  FooterComp
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
