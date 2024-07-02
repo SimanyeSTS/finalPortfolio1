29ed9386b55b9ddd8c511332c7953ef1d92a47be
SimanyeSTS <simanyesomdaka@gmail.com>
2024-07-02 10:07:34 +0200

    allCardsDone

---
+++ src/components/TestimonialsCard1.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card mb-3">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <div class="card-body">
+            <slot name="cardBody"></slot>
+          </div>
+        </div>
+        <div class="col-md-8">
+          <slot name="cardHeader"></slot>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'TestimonialsCard1',
+  };
+  </script>
+  
+  <style scoped>
+  .card{
+    max-width: 540px;
+  }
+
+  </style>
+  
---
+++ src/components/TestimonialsCard2.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card mb-3">
+      <div class="row g-0">
+        <div class="col-md-8">
+          <div class="card-body">
+            <slot name="cardBody"></slot>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <slot name="cardHeader"></slot>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'TestimonialsCard2',
+  };
+  </script>
+  
+  <style scoped>
+  .card{
+    max-width: 540px;
+  }
+
+  </style>
+  
