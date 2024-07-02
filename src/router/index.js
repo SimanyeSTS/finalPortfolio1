e0071c04d77f6f3850fdc6573a807de20dd2c250
SimanyeSTS <simanyesomdaka@gmail.com>
2024-07-02 12:26:27 +0200

    routerDoone

--- src/router/index.js
+++ src/router/index.js
@@ -5,37 +5,47 @@ import AboutSection from '@/components/AboutSection.vue'
 import ResumeSection from '@/components/ResumeSection.vue'
 import ProjectsSection from '@/components/ProjectsSection.vue'
 import TestimonialsSection from '@/components/TestimonialsSection.vue'
-import ContactSection from '@/component/ContactSection.vue'
+import ContactSection from '@/components/ContactSection.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: HomeView
-    children [
+    component: HomeView,
+    children: [
       {
         path: '',
         name: 'HomeSection',
         component: HomeSection  
       },
-      path: 'About',
-      name: 'AboutSection'
-      
+      {
+        path: 'about',
+        name: 'AboutSection',
+        component: AboutSection
+      },
+      {
+        path: 'resume',
+        name: 'ResumeSection',
+        component: ResumeSection
+      },
+      {
+        path: 'projects',
+        name: 'ProjectsSection',
+        component: ProjectsSection
+      },
+      {
+        path: 'testimonials',
+        name: 'TestimonialsSection',
+        component: TestimonialsSection
+      },
+      {
+        path: 'contact',
+        name: 'ContactSection',
+        component: ContactSection
+      }
     ]
-  },
-  {
-   
-
   }
 ]
-
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
-
-export default router
-
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,24 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+import Swal from 'sweetalert2'
+const porftfolioURL = 'https://simanyests.github.io/myPorfolioData/data/'
 
 export default createStore({
   state: {
+    jobTitle: null,
+    about: null,
+    resume: null,
+    projects: null,
+    testimonials: null
   },
   getters: {
   },
   mutations: {
+    setjobTitle(state, value) {
+    state.jobTitle = value
+  },
+  setabout(state, value) {
+    state.about = value
   },
   actions: {
   },
