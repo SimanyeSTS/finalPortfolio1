40549cc92156850ec00adeae531a402075a98100
SimanyeSTS <simanyesomdaka@gmail.com>
2024-07-03 17:01:38 +0200

    not done

--- src/components/TestimonialsSection.vue
+++ src/components/TestimonialsSection.vue
@@ -1,14 +1,49 @@
 <template>
-    <div>
-        
+    <div class="testimonials">
+      <h1>Testimonials</h1>
+      <ul>
+        <li v-for="testimonial in testimonials" :key="testimonial.id">
+          <component
+            :is="testimonial.id % 2 === 0 ? 'TestimonialsCard1' : 'TestimonialsCard2'"
+            :testimonial="testimonial"
+          >
+            <template v-slot:cardHeader>
+              <h2>{{ testimonial.title }}</h2>
+            </template>
+            <template v-slot:cardBody>
+              <p>{{ testimonial.description }}</p>
+            </template>
+          </component>
+        </li>
+      </ul>
     </div>
-</template>
-
-<script setup>
-
-</script>
-
-<style lang="scss" scoped>
-
-
-</style>
+  </template>
+  <script>
+  import { mapState, mapActions } from 'vuex';
+  export default {
+    name: 'TestimonialsSection',
+    computed: {
+      ...mapState({
+        testimonials: state => state.testimonials
+      })
+    },
+    created() {
+      this.fetchTestData();
+    },
+    methods: {
+      ...mapActions(['fetchTestimonials'])
+    }
+  };
+  </script>
+  <style scoped>
+  .testimonials {
+    padding: 20px;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+  li {
+    margin-bottom: 30px;
+  }
+  </style>
