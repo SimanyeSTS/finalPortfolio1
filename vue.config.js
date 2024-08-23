4f2a5780f509807c54cb5841f91bd6549771d764
SimanyeSTS <simanyesomdaka@gmail.com>
2024-08-23 12:32:46 +0200

    redefinement-removed favicon in config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  favicon: 'https://simanyests.github.io/Images/images/Screenshot 2024-05-07 141157.png'
+  transpileDependencies: true
 })
 
 
